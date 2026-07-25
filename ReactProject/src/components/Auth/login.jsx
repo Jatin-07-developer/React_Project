dd2a018b9170d9f40a7f69853e0217cb080f7601
Jatin-07-developer <animatedworld434@gmail.com>
2026-07-25 21:34:49 +0530

    login page update

--- ReactProject/src/components/Auth/login.jsx
+++ ReactProject/src/components/Auth/login.jsx
@@ -1,8 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const login = () => {
+
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
     const submitHandler = (e) => {
         e.preventDefault()
+        console.log("email is",email)
+        console.log("password is", password)
+
+        setEmail('')
+        setPassword('')
     }
 
   return (
@@ -11,8 +20,12 @@ const login = () => {
             <form onSubmit={(e)=>{
               submitHandler(e)
             }} className='flex flex-col items-center justify-center'>
-                <input required className='m-2 outline-none bg-transparent placeholder:text-gray-400 border-2 border-emerald-600 py-4 px-5 text-xl rounded-full text-white' type="email" placeholder='Enter your email' />
-                <input required className='outline-none bg-transparent placeholder:text-gray-400 border-2 border-emerald-600 py-4 px-5 text-xl text-white rounded-full' type="password" placeholder='Enter your password' />
+                <input value={email} onChange={(e)=>{
+                  setEmail(e.target.value)
+                }} required className='m-2 outline-none bg-transparent placeholder:text-gray-400 border-2 border-emerald-600 py-4 px-5 text-xl rounded-full text-white' type="email" placeholder='Enter your email' />
+                <input value={password} onChange={(e)=>{
+                  setPassword(e.target.value)
+                }} required className='outline-none bg-transparent placeholder:text-gray-400 border-2 border-emerald-600 py-4 px-5 text-xl text-white rounded-full' type="password" placeholder='Enter your password' />
                 <button className=' mt-5 text-white outline-none placeholder:text-white border-none bg-emerald-600 py-2 px-4 text-xl rounded-full w-68'>LogIn</button>
             </form>
         </div>
