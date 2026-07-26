61083bbb97c04762096ac2e91e1c964eeef047ae
Jatin-07-developer <animatedworld434@gmail.com>
2026-07-26 12:19:30 +0530

    TaskListNumbers and TaskList modify

--- ReactProject/src/components/other/TaskListNumbers.jsx
+++ ReactProject/src/components/other/TaskListNumbers.jsx
@@ -1,13 +1,26 @@
-import React from 'react'
+import React from "react";
 
 const TaskListNumbers = () => {
   return (
-    <div className='flex justify-between mt-10 gap-5 h-screen'>
-        <div className='h-40 w[45%] bg-red-400'>
-
-        </div>
+    <div className="flex justify-between mt-10 gap-5 screen">
+      <div className="rounded-xl h-30 w-[45%] bg-red-400 py-6 px-9">
+        <h2 className="text-3xl font-semibold  ">0</h2>
+        <h3 className="text-xl font-medium">New Task</h3>
+      </div>
+      <div className="rounded-xl h-30 w-[45%] bg-blue-400 py-6 px-9">
+        <h2 className="text-3xl font-semibold  ">0</h2>
+        <h3 className="text-xl font-medium">New Task</h3>
+      </div>
+      <div className="rounded-xl h-30 w-[45%] bg-green-400 py-6 px-9">
+        <h2 className="text-3xl font-semibold  ">0</h2>
+        <h3 className="text-xl font-medium">New Task</h3>
+      </div>
+      <div className="rounded-xl h-30 w-[45%] bg-yellow-400 py-6 px-9">
+        <h2 className="text-3xl font-semibold  ">0</h2>
+        <h3 className="text-xl font-medium">New Task</h3>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default TaskListNumbers
+export default TaskListNumbers;
