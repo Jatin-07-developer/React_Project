48db34309da1cf0e9b60217c12deb4b6f92edeb3
Jatin-07-developer <animatedworld434@gmail.com>
2026-07-26 14:27:11 +0530

    TaskList update

--- ReactProject/src/components/TaskList/TaskList.jsx
+++ ReactProject/src/components/TaskList/TaskList.jsx
@@ -2,8 +2,31 @@ import React from 'react'
 
 const TaskList = () => {
   return (
-    <div className='h-[55%] w-full bg-red-500 mt-10'>
+    <div id='tasklist' className='overflow-x-auto h-[55%] flex item-center justify-start gap-5 flex-nowrap w-full mt-10'>
+      <div className='shrink-0 h-full w-[75] border-y-yellow-400 rounded-xl'>
 
+      </div>
+      <div className='shrink-0 h-full w-[75] border-y-yellow-400 rounded-xl'>
+
+      </div>
+      <div className='shrink-0 h-full w-[75] border-y-yellow-400 rounded-xl'>
+
+      </div>
+      <div className='shrink-0 h-full w-[75] border-y-yellow-400 rounded-xl'>
+
+      </div>
+      <div className='shrink-0 h-full w-[75] border-y-yellow-400 rounded-xl'>
+
+      </div>
+      <div className='shrink-0 h-full w-[75] border-y-yellow-400 rounded-xl'>
+
+      </div>
+      <div className='shrink-0 h-full w-[75] border-y-yellow-400 rounded-xl'>
+
+      </div>
+      <div className='shrink-0 h-full w-[75] border-y-yellow-400 rounded-xl'>
+
+      </div>
     </div>
   )
 }
