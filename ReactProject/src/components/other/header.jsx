7dc3baf9cbdd7a419bd1e0b32e82fc617887dfbd
Jatin-07-developer <animatedworld434@gmail.com>
2026-07-29 19:13:40 +0530

    Header modify

--- ReactProject/src/components/other/header.jsx
+++ ReactProject/src/components/other/header.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Header = ({data}) => {
   return (
     <div className='flex items-end justify-between'>
-        <h1 className='text-2xl text-white font-medium'>Hello <br /> <span className='text-3xl text-white font-semibold'>Jatin 👋</span></h1>
+        <h1 className='text-2xl text-white font-medium'>Hello <br /> <span className='text-3xl text-white font-semibold'>{data.firstName} 👋</span></h1>
         <button className='bg-red-500 text-white rounded-s-sm text-lg font-medium px-5 py-2'>Log Out</button>
     </div>
   );
