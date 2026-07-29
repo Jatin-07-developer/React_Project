b6fce207b5b2fb4ba7dd2659503d1d0bd40e69af
Jatin-07-developer <animatedworld434@gmail.com>
2026-07-29 22:28:08 +0530

    TaskList update

--- ReactProject/src/components/TaskList/TaskList.jsx
+++ ReactProject/src/components/TaskList/TaskList.jsx
@@ -4,15 +4,26 @@ import NewTask from './NewTask'
 import CompleteTask from './CompleteTask'
 import FailedTask from './FailedTask'
 
-const TaskList = ({data}) => {
-  return (
-    <div id='tasklist' className='overflow-x-auto h-[45%] flex item-center justify-start gap-5 flex-nowrap w-full mt-10'>
-      <AcceptTask />
-      <NewTask />
-      <CompleteTask />
-      <FailedTask />
-    </div>
-  )
+const TaskList = ({ data }) => {
+    return (
+        <div id='tasklist' className='h-[50%] overflow-x-auto flex items-center justify-start gap-5 flex-nowrap w-full py-1 mt-16'>
+            {data.tasks.map((elem, idx) => {
+                if (elem.active) {
+                    return <AcceptTask key={idx} data={elem} />
+                }
+                if (elem.newTask) {
+                    return <NewTask key={idx} data={elem} />
+                }
+                if (elem.completed) {
+                    return <CompleteTask key={idx} data={elem} />
+                }
+                if (elem.failed) {
+                    return <FailedTask key={idx} data={elem} />
+                }
+
+            })}
+        </div>
+    )
 }
 
 export default TaskList
