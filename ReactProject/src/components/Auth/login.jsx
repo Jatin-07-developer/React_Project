6b66f8e45c95edeaf956858ea46cd7a7c4646cc1
Jatin-07-developer <animatedworld434@gmail.com>
2026-08-01 20:15:18 +0530

    Some changes in the design

--- ReactProject/src/components/Auth/login.jsx
+++ ReactProject/src/components/Auth/login.jsx
@@ -1,37 +1,50 @@
 import React, { useState } from 'react'
 
-const login = ({handleLogin}) => {
+const Login = ({handleLogin}) => {
+
+    
 
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
 
-    const submitHandler = (e) => {
-        e.preventDefault()
-        handleLogin(email, password)
-        // console.log("email is",email)
-        // console.log("password is", password)
 
-        setEmail('')
-        setPassword('')
+    const submitHandler = (e)=>{
+        e.preventDefault()
+        handleLogin(email,password)
+        setEmail("")
+        setPassword("")
     }
 
+
   return (
-    <div className='flex h-screen w-screen items-center justify-center bg-black'>
-        <div className='border-2 border-emerald-600 p-20 rounded-xl'>
-            <form onSubmit={(e)=>{
-              submitHandler(e)
-            }} className='flex flex-col items-center justify-center'>
-                <input value={email} onChange={(e)=>{
-                  setEmail(e.target.value)
-                }} required className='m-2 outline-none bg-transparent placeholder:text-gray-400 border-2 border-emerald-600 py-4 px-5 text-xl rounded-full text-white' type="email" placeholder='Enter your email' />
-                <input value={password} onChange={(e)=>{
-                  setPassword(e.target.value)
-                }} required className='outline-none bg-transparent placeholder:text-gray-400 border-2 border-emerald-600 py-4 px-5 text-xl text-white rounded-full' type="password" placeholder='Enter your password' />
-                <button className=' mt-5 text-white outline-none placeholder:text-white border-none bg-emerald-600 py-2 px-4 text-xl rounded-full w-68'>LogIn</button>
+    <div className='flex h-screen w-screen items-center justify-center'>
+        <div className='border-2 rounded-xl border-emerald-600 p-20'>
+            <form 
+            onSubmit={(e)=>{
+                submitHandler(e)
+            }}
+            className='flex flex-col items-center justify-center'
+            >
+                <input 
+                value={email}
+                onChange={(e)=>{
+                    setEmail(e.target.value)
+                }}
+                required 
+                className='outline-none bg-transparent border-2 border-emerald-600 font-medium text-lg py-2 px-6 rounded-full placeholder:text-gray-400' type="email" placeholder='Enter your email' 
+                />
+                <input
+                value={password}
+                onChange={(e)=>{
+                    setPassword(e.target.value)
+                }}
+                required 
+                className='outline-none bg-transparent border-2 border-emerald-600 font-medium text-lg py-2 px-6 rounded-full mt-3 placeholder:text-gray-400' type="password" placeholder='Enter password' />
+                <button className='mt-7 text-white border-none outline-none hover:bg-emerald-700 font-semibold bg-emerald-600 text-lg py-2 px-8 w-full rounded-full placeholder:text-white'>Log in</button>
             </form>
         </div>
     </div>
   )
 }
 
-export default login
+export default Login
--- ReactProject/src/utils/localStorage.jsx
+++ ReactProject/src/utils/localStorage.jsx
@@ -1,3 +1,4 @@
+
 const employees = [
     {
         "id": 1,
@@ -202,21 +203,19 @@ const employees = [
 ];
 
 
-const admin = [ { 
-    "id": 101, 
-    "email": "admin@example.com", 
-    "password": "123" 
-} ]
+const admin = [{
+    "id": 1,
+    "email": "admin@example.com",
+    "password": "123"
+}];
 
-export const setLocalStorage = () => {
-    localStorage.setItem('employees', JSON.stringify(employees))
-    localStorage.setItem('admin', JSON.stringify(admin))
-
+export const setLocalStorage = ()=>{
+    localStorage.setItem('employees',JSON.stringify(employees))
+    localStorage.setItem('admin',JSON.stringify(admin))
 }
-export const getLocalStorage = () =>{
+export const getLocalStorage = ()=>{
     const employees = JSON.parse(localStorage.getItem('employees'))
     const admin = JSON.parse(localStorage.getItem('admin'))
 
-    return {employees, admin}
-    
+    return {employees,admin}
 }
